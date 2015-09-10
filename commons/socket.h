be544427a483c33cc1c651b20a8c43653d14dccc
Adrian Guibu <guibuadrian@gmail.com>
2015-09-10 11:58:29 -0300

    Le agrego las commons de sockets

---
+++ commons/socket.h
@@ -0,0 +1,10 @@
+#ifndef SOCKET_H_
+#define SOCKET_H_
+
+int conectarse(char * IP, char * PUERTO_SERVIDOR);
+
+int recibirLlamada(char * IP, char * PUERTO_ESCUCHA);
+
+int aceptarLlamada(int listenningSocket);
+
+#endif
